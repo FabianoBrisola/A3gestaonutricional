4e14decafd53691f4da991fb5c2e0b234b5425be
FabianoBrisola <133615951+FabianoBrisola@users.noreply.github.com>
2023-06-14 19:55:56 -0300

    Conexão
    editando conexão BD

--- DentrodoApp/src/ConexaoBD.java
+++ DentrodoApp/src/ConexaoBD.java
@@ -5,10 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConexaoBD {
-    private static final String URL = "jdbc:mysql://localhost:3306/nome_do_banco_de_dados";
-    private static final String USUARIO = "seu_usuario";
-    private static final String SENHA = "sua_senha";
-
+    private static final String URL = "jdbc:mysql://localhost:3306/sgn";
+    private static final String USUARIO = "root";
+    private static final String SENHA = "";
     public static Connection obterConexao() throws SQLException {
         return DriverManager.getConnection(URL, USUARIO, SENHA);
     }
@@ -17,12 +16,11 @@ public class ConexaoBD {
         try {
             
             Connection conexao = ConexaoBD.obterConexao();
-            System.out.println("Conexão estabelecida com sucesso!");
-            // Faça operações com o banco de dados usando a conexão
-            // ...
-            conexao.close(); // Feche a conexão quando não for mais necessária
+            System.out.println("Conexao estabelecida com sucesso!");
+            
+            conexao.close(); 
         } catch (SQLException e) {
-            System.out.println("Erro ao estabelecer conexão com o banco de dados:");
+            System.out.println("Erro ao estabelecer conexao com o banco de dados:");
             e.printStackTrace();
         }
     }
